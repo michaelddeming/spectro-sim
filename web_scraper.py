3839bd1acf186e8330bde06d52f47031ba530b9d
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-05-17 17:53:45 -0700

    CID request via name function successful.

---
+++ web_scraper.py
@@ -0,0 +1,32 @@
+import  requests
+
+
+
+
+def get_cid(name: str) -> int:
+
+    """Given the name of a compound, search PubChem via PUG Rest API for the CID for the compound within the online database. 
+    
+    PUG Rest API returns a response obj of minimal data related to a compound, like its CID.'
+
+    Returns the CID of the compound name provided, as an int. 
+    """
+    
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{name}/cids/JSON"
+
+    response = requests.get(url)
+
+    try:      
+        compound_data = response.json()
+        return compound_data["IdentifierList"]["CID"][0]
+    except KeyError:
+        print("Error, Compound Name Invalid.")
+    except TypeError:
+        print("Error 404, Invalid Search Reponse.")
+
+
+
+
+# def get_data_via_cid(cid:)
+
+print(get_cid("asprin"))
