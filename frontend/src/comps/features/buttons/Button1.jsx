5325ec39213a3866fd5cfc0b474ab95bbdd741f5
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-05-26 02:04:13 -0700

    Styled Compound Search section.

--- frontend/src/comps/features/buttons/Button1.jsx
+++ frontend/src/comps/features/buttons/Button1.jsx
@@ -3,7 +3,7 @@ export default function Button1(props){
     return(
         <>
 
-        <button className="bg-cyan-600 not-only:px-4 py-1 rounded-xl cursor-pointer shadow-[4px_4px_8px_rgba(0,0,0,0.3)] hover:bg-slate-900 transition duration-200">{props.text}
+        <button className="bg-cyan-700 text-cyan-200 px-6 py-1 rounded-xl cursor-pointer shadow-[4px_4px_8px_rgba(0,0,0,0.3)] hover:bg-cyan-500 hover:text-slate-200 transition duration-200">{props.text}
             </button>
 
         </>
--- frontend/src/comps/pages/AbsorbSim.jsx
+++ frontend/src/comps/pages/AbsorbSim.jsx
@@ -1,12 +1,23 @@
 import Button1 from "../features/buttons/button1";
+import CompoundInput from "../features/CompoundInput";
 import Nav from "../features/Nav";
 
 function AbsorbSim() {
     return(
         <>
         <Nav></Nav>
-        <Button1
-        text="SEARCH"></Button1>
+        <div>
+            <div className="flex flex-col items-center gap-2 pt-30">
+                <h1 className="text-3xl">Compound Search</h1>
+                <div className="flex flex-row gap-4 w-fit">
+                    <CompoundInput></CompoundInput>
+                    <Button1 text="SEARCH"></Button1>
+                </div>
+                <p style={{color:"#DFFCFD"}}className="text-sm mt-3">Spectral data sourced from the <a href="https://pubchem.ncbi.nlm.nih.gov" className="underline hover:text-cyan-400">PubChem</a> database.</p>
+            </div>
+        </div>
+        
+        
 
         </>
     );
