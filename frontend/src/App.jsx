8a81545d4917dc00c035cf0cf1c79416d0cd44b3
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-05-25 20:10:16 -0700

    Set up routes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
     <>
-  
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </Router>
+      
     
 
     </>
