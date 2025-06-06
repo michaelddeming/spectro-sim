fdd6da377002d049b3f09cc0e9f8d3809bcec82d
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-06-05 20:14:11 -0700

    Add react-icons. Change SEARCH to search icon.

--- frontend/src/comps/features/buttons/CompoundSearchButton.jsx
+++ frontend/src/comps/features/buttons/CompoundSearchButton.jsx
@@ -1,9 +1,17 @@
+import { FiSearch } from "react-icons/fi";
+
+
+
 
 export default function CompoundSearchButton(props){
     return(
         <>
 
-        <button className="bg-cyan-700 text-cyan-200 px-6 py-1 rounded-xl cursor-pointer shadow-[4px_4px_8px_rgba(0,0,0,0.5)] hover:bg-cyan-500 hover:text-slate-200 transition duration-200" onClick={props.onClick}>{props.text}
+        <button className="bg-cyan-700 text-cyan-200 px-6 py-1 rounded-xl cursor-pointer shadow-[4px_4px_8px_rgba(0,0,0,0.5)] hover:bg-cyan-500 hover:text-slate-200 transition duration-200 h-[40px]" onClick={props.onClick}>
+            <FiSearch 
+            className="
+            size-6
+            "></FiSearch>
             </button>
 
         </>
